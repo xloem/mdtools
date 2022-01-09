e04b62e4b2e5e67619317822eff872d9163942ac
xloem <0xloem@gmail.com>
2022-01-09 15:17:17 +0000

    read bytes in separate function for struct imaging

--- mdtools/namedfilestruct.py
+++ mdtools/namedfilestruct.py
@@ -7,7 +7,7 @@ class NamedFileStruct:
     def __init__(self, fileobj, offset = None, endian = None, **fields):
         self.struct = None
         self.read(fileobj, offset, endian, **fields)
-    def read(self, fileobj = None, offset = None, endian = None, **fields):
+    def read_bytes(self, fileobj = None, offset = None, endian = None, **fields):
         if fileobj is not None:
             self.fileobj = fileobj
         if offset is not None:
@@ -21,11 +21,14 @@ class NamedFileStruct:
             self.struct = struct.Struct(self.endian + ''.join(self.fields.values()))
         if self.offset is not None:
             self.fileobj.seek(self.offset)
+        return self.fileobj.read(self.struct.size)
+    def read(self, fileobj = None, offset = None, endian = None, **fields):
+        bytes = self.read_bytes(fileobj, offset, endian, **fields)
         self.values = {
             key : val
             for key, val in zip(
                 self.fields.keys(), 
-                self.struct.unpack(self.fileobj.read(self.struct.size))
+                self.struct.unpack(bytes)
             )
         }
     def __getattr__(self, name):
