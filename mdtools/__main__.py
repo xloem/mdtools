8a8975006e1577d4686ef60d8af51d6aeb1db948
xloem <0xloem@gmail.com>
2022-01-11 13:37:00 +0000

    verbose flag

--- mdtools/__main__.py
+++ mdtools/__main__.py
@@ -4,6 +4,7 @@ from .superblock import Superblock
 
 parser = argparse.ArgumentParser(description='put tool description here')
 #parser.add_argument('-d', '--dump', help='write superblocks to local files')
+parser.add_argument('-v', '--verbose', action='store_true', help='list all superblock properties')
 parser.add_argument('paths', nargs='+', help='help for paths argument')
 args = parser.parse_args()
 
@@ -14,4 +15,12 @@ for path in args.paths:
         except Exception as e:
             print(path, e)
             continue
-        print(path, block.dev_number, block.events)
+        if args.verbose:
+            print(path)
+            for name, value in block.values.items():
+                if name == 'dev_roles':
+                    while value[-1] == 65535:
+                        value = value[:-1]
+                print(f' {name}: {value}')
+        else:
+            print(path, block.dev_number, block.events)
